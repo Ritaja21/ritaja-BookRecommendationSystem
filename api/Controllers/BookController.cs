b1afe0b479109ce7248d2008cd87a70db5ab0846
Ritaja <tonnitarafder2004@gmail.com>
2026-04-23 12:42:14 +0530

    Added GetBookById Api

--- api/Controllers/BookController.cs
+++ api/Controllers/BookController.cs
@@ -21,9 +21,35 @@ namespace api.Controllers
 
         public async Task<ActionResult<IEnumerable<Book>>> GetBook()
         {
-            return await _db.Books.ToListAsync();
+            return Ok(await _db.Books.ToListAsync());
         }
 
         //get books by id
+
+        [HttpGet("{id:int}")]
+
+        public async Task<ActionResult<Book>> GetBookById(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    return BadRequest("Movie ID must be greater than 0");
+                }
+
+                var book = await _db.Books.FirstOrDefaultAsync(u=> u.BookId == id);
+                if(book == null)
+                {
+                    return NotFound($"Movie with ID{id} was not found");
+                }
+                return Ok(book);
+
+            }
+            catch (Exception ex) 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, 
+                    $"An error occured while retireving book with ID {id}: {ex.Message}");
+            }
+        }
     }
 }
