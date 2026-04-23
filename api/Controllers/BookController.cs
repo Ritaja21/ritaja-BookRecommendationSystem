73d1a62b66bba8edfce143b0325c3d2fc97882a6
Ritaja <tonnitarafder2004@gmail.com>
2026-04-23 12:29:03 +0530

    Added GetBook Api

---
+++ api/Controllers/BookController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using api.Data;
+using api.Models;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/book")]
+    public class BookController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        public BookController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+
+        //get books api
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<Book>>> GetBook()
+        {
+            return await _db.Books.ToListAsync();
+        }
+
+        //get books by id
+    }
+}
