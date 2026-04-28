1bd978bcffad107bd8b0a54b32376939e5f1e8f7
Ritaja <tonnitarafder2004@gmail.com>
2026-04-28 20:19:49 +0530

    Added UsersTable

---
+++ api/src/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.src.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public required string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+        [Required]
+        public required string Password { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public required string Role { get; set; } = "Customer";
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
