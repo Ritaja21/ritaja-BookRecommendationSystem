ef4830e9a495d6a62cfbfff1974850ed8be463a8
Ritaja <tonnitarafder2004@gmail.com>
2026-05-07 10:53:13 +0530

    Added Login and register DTOs for users

---
+++ api/src/Models/DTO/LoginRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.src.Models.DTO
+{
+    public class LoginRequestDTO
+    {
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+        [Required]
+        public required string Password { get; set; }
+    }
+}
---
+++ api/src/Models/DTO/LoginResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace api.src.Models.DTO
+{
+    public class LoginResponseDTO
+    {
+        public string? Token { get; set; }
+
+        public UserDTO? UserDTO { get; set; }
+    }
+}
