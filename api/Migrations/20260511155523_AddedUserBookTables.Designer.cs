21a395519c7dea782addbb1da3482a60a9559350
Ritaja <tonnitarafder2004@gmail.com>
2026-05-11 21:27:14 +0530

    feat: added userbook table

--- api/Migrations/20260511155523_AddedUserBookTables.Designer.cs
+++ api/Migrations/20260511155523_AddedUserBookTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using api.src.Data;
 
@@ -11,9 +12,11 @@ using api.src.Data;
 namespace api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260511155523_AddedUserBookTables")]
+    partial class AddedUserBookTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ api/src/Models/UserBook.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.src.Models
+{
+    public class UserBook
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public int BookId { get; set; }
+        [Range(1,5)]
+        public int? Rating { get; set; }
+        public bool IsRead { get; set; } = false;
+
+        [ForeignKey("UserId")]
+        public User User { get; set; } = null!;
+
+        [ForeignKey("BookId")]
+        public Book Book { get; set; } = null!;
+    }
+}
