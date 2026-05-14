2868dce799db116f8e506ac825460495c1ac41c1
Ritaja <tonnitarafder2004@gmail.com>
2026-05-14 12:46:14 +0530

    feat : created different recommendation DTOs

---
+++ api/src/Models/DTO/RecommendationBookDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace api.src.Models.DTO
+{
+    public class RecommendationBookDTO
+    {
+        public string Title { get; set; } = string.Empty;
+        public string? Author {  get; set; } = string.Empty;
+        public string? Genre { get; set; }
+        public bool IsInternal { get; set; }
+        public string? SearchUrl { get; set; }
+
+    }
+}
