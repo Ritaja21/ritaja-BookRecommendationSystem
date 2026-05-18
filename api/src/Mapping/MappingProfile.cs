ac66cf25787f6523bf0888516a0cb412a1cf71bf
Ritaja <tonnitarafder2004@gmail.com>
2026-05-18 22:56:29 +0530

    feat : added Auto Mapper profiles

---
+++ api/src/Mapping/MappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using api.src.Models;
+using api.src.Models.DTO;
+using AutoMapper;
+
+namespace api.src.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() {
+            CreateMap<BookCreateDTO, Book>();
+            CreateMap<BookUpdateDTO, Book>();
+            CreateMap<Book, BookDTO>()
+             .ForMember(dest => dest.AverageRating,
+                 opt => opt.MapFrom(src =>
+                     src.UserBooks.Any(ub => ub.Rating.HasValue)
+                         ? Math.Round(
+                             src.UserBooks
+                                 .Where(ub => ub.Rating.HasValue)
+                                 .Average(ub => ub.Rating!.Value),
+                             2)
+                         : (double?)null
+                 ));
+            CreateMap<User, UserDTO>();
+            CreateMap<UserBook, UserHistoryDTO>()
+            .ForMember(dest => dest.Title,
+                opt => opt.MapFrom(src => src.Book.Title))
+            .ForMember(dest => dest.Author,
+                opt => opt.MapFrom(src => src.Book.Author));
+        }
+    }
+}
