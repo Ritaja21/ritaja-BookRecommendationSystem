5d24db37304cfc48526029f13590c9f738098b84
Ritaja <tonnitarafder2004@gmail.com>
2026-05-27 12:26:23 +0530

    feat : added login integration

---
+++ ui/src/app/core/services/auth.services.ts
@@ -0,0 +1,58 @@
+import { inject, Inject, Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { Observable, tap } from "rxjs";
+import { environment } from "../../../environments/environment";
+
+import { LoginRequest } from "../models/auth/login-request.model";
+import { RegisterRequest } from "../models/auth/register-request.model";
+import { LoginResponse } from "../models/auth/login-response.model";
+import { User } from "../models/auth/user.model";
+import { ApiResponse } from "../models/auth/api-response.model";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private http = inject(HttpClient);
+
+  private apiUrl = environment.apiUrl;
+
+  register(data: RegisterRequest): Observable<ApiResponse<User>> {
+    return this.http.post<ApiResponse<User>>(
+      `${this.apiUrl}/auth/register`, data
+    );
+  }
+
+  login(data: LoginRequest): Observable<ApiResponse<LoginResponse>> {
+    return this.http.post<ApiResponse<LoginResponse>>(
+      `${this.apiUrl}/auth/login`, data).pipe(tap((response) => {
+
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('user', JSON.stringify(response.data.userDTO));
+      })
+      );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
+  getUser(): User | null {
+    const user = localStorage.getItem('user');
+    return user ? JSON.parse(user) : null;
+  }
+
+}
+
--- ui/src/app/features/auth/login/login.component.ts
+++ ui/src/app/features/auth/login/login.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
+
+import { AuthService } from '../../../core/services/auth.services';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, RouterLink, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  email = '';
+  password = '';
+  errorMessage = '';
+
+  onLogin() {
+    const loginData = {
+      email: this.email,
+      password: this.password
+    };
+
+    this.authService.login(loginData).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.router.navigate(['/']);
+      },
+      error: (error) => {
+        console.log(error);
+        this.errorMessage = error.error.message;
+      }
+
+    });
+  }
 
 }
