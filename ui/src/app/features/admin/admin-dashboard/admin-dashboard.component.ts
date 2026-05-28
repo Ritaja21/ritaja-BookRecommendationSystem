ad2fba5f3565025184b6c77be98db56ce4c4093d
Ritaja <tonnitarafder2004@gmail.com>
2026-05-28 11:35:42 +0530

    feat : added admin dashboard

--- ui/src/app/features/admin/admin-dashboard/admin-dashboard.component.ts
+++ ui/src/app/features/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.services';
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
 export class AdminDashboardComponent {
+  private authService = inject(AuthService);
 
+  user = this.authService.getUser();
+
+  getInitials(): string {
+    if (!this.user?.name) return '?';
+    return this.user.name.split(' ')
+      .map((n: string) => n[0]).join('').toUpperCase().slice(0, 2);
+  }
 }
