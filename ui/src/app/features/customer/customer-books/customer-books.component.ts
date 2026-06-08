87145968ddd74a4ef93cddc4cea51146bbe3c44b
Ritaja <tonnitarafder2004@gmail.com>
2026-06-08 12:06:06 +0530

    feat: updated browse book feature

--- ui/src/app/features/customer/customer-books/customer-books.component.ts
+++ ui/src/app/features/customer/customer-books/customer-books.component.ts
@@ -1,11 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIf, NgFor, DecimalPipe } from '@angular/common';
+import { BookService } from '../../../core/services/book.services';
+import { Book } from '../../../core/models/books/book.model';
+
 
 @Component({
   selector: 'app-customer-books',
-  imports: [],
+  imports: [FormsModule, NgIf, NgFor, DecimalPipe],
   templateUrl: './customer-books.component.html',
   styleUrl: './customer-books.component.css'
 })
-export class CustomerBooksComponent {
+export class CustomerBooksComponent implements OnInit {
+  private bookService = inject(BookService);
 
+  books: Book[] = [];
+  searchQuery = '';
+  genreFilter = '';
+  errorMessage = '';
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+    this.bookService.getBooks().subscribe({
+      next: (response) => {
+        this.books = response.data;
+      },
+      error: (error) => {
+        this.errorMessage = error.error?.message ?? 'Failed to load books';
+      }
+    });
+  }
+
+  search() {
+    if (!this.searchQuery && !this.genreFilter) {
+      this.loadBooks();
+      return;
+    }
+    this.bookService.searchBooks({
+      query: this.searchQuery,
+      genre: this.genreFilter
+    }).subscribe({
+      next: (response) => {
+        this.books = response.data;
+      },
+      error: (error) => {
+        this.errorMessage = error.error?.message ?? 'Searched Failed';
+      }
+    });
+  }
+
+  clearSearch() {
+    this.searchQuery = '';
+    this.genreFilter = '';
+    this.loadBooks();
+  }
+
+  getStars(rating: number): string {
+    return '★'.repeat(Math.round(rating));
+  }
+
+  getEmptyStars(rating: number): string {
+    return '★'.repeat(5 - Math.round(rating));
+  }
 }
